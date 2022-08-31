17e1c97a2029615b796c3213a31052009c5f13fe
scenno <46051985+scenno@users.noreply.github.com>
2022-08-31 10:30:30 +0800

    Update config.py

--- config.py
+++ config.py
@@ -19,7 +19,7 @@ DEFAULT_HEADERS = {
 调试用 COOKIE
 """
 TEST_COOKIE = ''
-
+SMZDM_COOKIE = '__ckguid=j4RL67ThFQ3o17o4whSo17; device_id=21307064331661912265561660e2a509b3ca5f53996501fcc3776ba7ca; homepage_sug=c; r_sort_type=score; __jsluid_s=16238cf32e0f3e5dca893b66c8864fb3; _zdmA.vid=*; sajssdk_2015_cross_new_user=1; footer_floating_layer=0; ad_date=31; ad_json_feed={}; sensorsdata2015jssdkcross={"distinct_id":"182f1b0bd8369d-07cd917c82ca02-26021c51-2073600-182f1b0bd84910","first_id":"","props":{"$latest_traffic_source_type":"自然搜索流量","$latest_search_keyword":"未取到值","$latest_referrer":"https://www.google.com/"},"$device_id":"182f1b0bd8369d-07cd917c82ca02-26021c51-2073600-182f1b0bd84910"}; sess=BA-g9e7O99cCD7LxIcOyPaH3dxfeYlGXueexnQDahmB5ilwj2yBxnbvoqvc0wh8G5aWiz9GmbASeDvGu1tEz+nidEYDU4vh44rtUbNXfjdH/+U/mbWZzSOvBuYA; user=scenno|8056710716; _zdmA.uid=ZDMA.ga8WgmO8Q.1661912373.2419200; bannerCounter=[{"number":0,"surplus":1},{"number":0,"surplus":1},{"number":1,"surplus":1},{"number":0,"surplus":1},{"number":1,"surplus":1},{"number":0,"surplus":1}]; smzdm_id=8056710716; amvid=d07c31f8382895205cd878e5aad0fb64; _zdmA.time=1661912373838.0.https://www.smzdm.com/'
 
 """
 调试用 SERVERCHAN_SECRETKEY
